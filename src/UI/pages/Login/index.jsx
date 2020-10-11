923bce0f7ecf0692b9fedb9d8bbb3942f443811d
Gustavo <guga.nd@hotmail.com>
2020-10-11 14:41:44 -0500

    Login Fixed UI
    Login UI without errors.

--- src/UI/pages/Login/index.jsx
+++ src/UI/pages/Login/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import TextBox from 'UI/components/atoms/TextBox';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyle = makeStyles({
+  root: {
+    background: 'linear-gradient(270deg, #ED8A9C 0%, #F5C4A1 100%)',
+    boxShadow: '0px 3px 6px #00000029'
+  }
+});
 export default function LogIn() {
-<<<<<<< Updated upstream
-=======
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -17,7 +24,6 @@ export default function LogIn() {
       .get(url)
       .then(response => {
         // handle success
-        console.log('succes', response);
         if (response.data === '1') {
           alert('Login successful!');
           setPassword('');
@@ -42,7 +48,6 @@ export default function LogIn() {
   };
 
   const classes = useStyle();
->>>>>>> Stashed changes
   return (
     <body style={{ background: '#FDF7F4', opacity: '1' }}>
       <div
@@ -62,60 +67,66 @@ export default function LogIn() {
               borderTopLeftRadius: 26,
               borderTopRightRadius: 26,
               borderBottomLeftRadius: 26,
-              borderBottomRightRadius: 26,
-              boxShadow: '0px 3px 6px #00000029'
+              borderBottomRightRadius: 26
             }}
             bgcolor="#FFFFFF"
           >
-            <center>
-              <h1
-                style={{
-                  marginTop: '54px',
-                  marginBottom: '55px',
-                  font: 'normal normal bold 32px/48px Poppins',
-                  color: '#94A6B3',
-                  height: '45px',
-                  letterspacing: '0px'
-                }}
-              >
-                INICIAR SESIÓN
-              </h1>
-              <TextBox style={{ width: '315px', height: '40px' }} name="Usuario" label="Usuario" />
-              <TextBox
-                style={{ marginTop: '34px', width: '315px', height: '40px' }}
-                name="Contraseña"
-                label="Contraseña"
-                type="password"
-              />
-
-              <Button
-                style={{
-                  marginTop: '42px',
-                  marginBottom: '58px',
-                  width: '171px',
-                  color: 'white',
-                  height: '48px',
-                  backgroundColor: '#F5C4A1',
-                  borderTopLeftRadius: 24,
-                  borderTopRightRadius: 24,
-                  borderBottomLeftRadius: 24,
-                  borderBottomRightRadius: 24
-                }}
-              >
+            <form onSubmit={onSubmit}>
+              <center>
                 <h1
                   style={{
-                    width: '61px',
-                    font: ' 16px/19px Roboto',
-                    marginTop: '15px',
-                    marginBottom: '14px',
-                    opacity: '1',
+                    marginTop: '54px',
+                    marginBottom: '55px',
+                    font: 'normal normal bold 32px/48px Poppins',
+                    color: '#94A6B3',
+                    height: '45px',
                     letterspacing: '0px'
                   }}
                 >
-                  ENTRAR
+                  INICIAR SESIÓN
                 </h1>
-              </Button>
-            </center>
+                <TextBox
+                  onChange={onChangeu}
+                  style={{ width: '315px', height: '40px' }}
+                  name="Usuario"
+                  label="Usuario"
+                />
+                <TextBox
+                  onChange={onChangep}
+                  style={{ marginTop: '34px', width: '315px', height: '40px' }}
+                  name="Contraseña"
+                  label="Contraseña"
+                  type="password"
+                />
+                <Button
+                  className={classes.root}
+                  style={{
+                    marginTop: '42px',
+                    marginBottom: '58px',
+                    width: '171px',
+                    color: 'white',
+                    height: '48px',
+                    borderTopLeftRadius: 24,
+                    borderTopRightRadius: 24,
+                    borderBottomLeftRadius: 24,
+                    borderBottomRightRadius: 24
+                  }}
+                >
+                  <h1
+                    style={{
+                      width: '61px',
+                      font: ' 16px/19px Roboto',
+                      marginTop: '15px',
+                      marginBottom: '14px',
+                      opacity: '1',
+                      letterspacing: '0px'
+                    }}
+                  >
+                    ENTRAR
+                  </h1>
+                </Button>
+              </center>
+            </form>
           </Box>
         </Box>
       </div>
