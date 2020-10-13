f37bb884f153ed3395781847b1ce8b1834c46e17
Gustavo <guga.nd@hotmail.com>
2020-10-13 01:40:29 -0500

    Responsive and Styles Login

--- src/UI/pages/Login/styles.js
+++ src/UI/pages/Login/styles.js
@@ -1,54 +1,61 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { colors } from 'UI/res';
 
-export const styles = {
-  backgroundImg: {
-    zIndex: 0,
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    height: `100%`
+export const useStyles = makeStyles({
+  boton: {
+    background: 'linear-gradient(270deg, #ED8A9C 0%, #F5C4A1 100%)',
+    boxShadow: '0px 3px 6px #00000029',
+    marginTop: '42px',
+    marginBottom: '58px',
+    width: '171px',
+    color: 'white',
+    height: '48px',
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24
   },
-
-  pattern1: {
-    top: 0,
-    left: 0
-  },
-  pattern2: {
-    bottom: 0,
-    right: 0
-  }
-};
-export const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: colors.appBackground,
-    height: '100%',
-    width: '100%'
-  },
-  pattern: {
-    position: 'absolute',
-    margin: 8
-  },
-  wrapper: {
-    zIndex: 10,
-    backgroundColor: colors.transparent,
-    height: '100%',
+  fondo: {
+    background: '#FDF7F4',
     width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    position: 'relative'
+    height: '100%',
+    opacity: '1'
   },
-  backgroundImg: {
-    ...styles.backgroundImg
+  box: {
+    width: '398px',
+    marginTop: '11%',
+    height: '438px',
+    boxShadow: '0px 3px 6px #00000029',
+    borderTopLeftRadius: 26,
+    borderTopRightRadius: 26,
+    borderBottomLeftRadius: 26,
+    borderBottomRightRadius: 26,
+    background: 'white'
   },
-  label: {
-    maxWidth: '45vw',
-    [theme.breakpoints.down('sm')]: {
-      maxWidth: '75vw'
-    },
-    textAlign: 'center'
+  h1: {
+    marginTop: '54px',
+    marginBottom: '55px',
+    font: 'normal normal bold 32px/48px Poppins',
+    color: '#94A6B3',
+    height: '45px',
+    letterspacing: '0px'
+  },
+  txtusuario: {
+    width: '315px',
+    height: '40px'
+  },
+  txtcontrasena: {
+    marginTop: '34px',
+    width: '315px',
+    height: '40px'
+  },
+  txtboton: {
+    width: '61px',
+    font: '16px/19px Roboto',
+    marginTop: '15px',
+    marginBottom: '14px',
+    opacity: '1',
+    height: '19px',
+    color: '#FCFCFC',
+    letterspacing: '0px'
   }
-}));
+});
