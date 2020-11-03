469339af063144e39703ef3547fced22e5d72a5b
Ivan Pacheco <bytepacheco@gmail.com>
2020-11-02 19:10:40 -0600

    Try to implement state with language

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 // @flow
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'App.css';
 import { THEME } from 'GlobalStyles';
 import Routes from 'routes/Routes';
 
-// const languageStoredInLocalStorage = localStorage.getItem('language');
-
-// const [language, setLanguage] = useState(languageStoredInLocalStorage || 'English');
+function storeLanguageInLocalStorage(language) {
+  localStorage.setItem('language', language);
+}
 
 const App = () => {
+  const languageStoredInLocalStorage = localStorage.getItem('language');
+  const [language, setLanguage] = useState(languageStoredInLocalStorage || 'English');
+
   useEffect(() => {
     localStorage.setItem('language', 'Spanish');
   }, []);
   return (
     <MuiThemeProvider theme={THEME}>
-      <Routes />
+      <Routes
+        language={language}
+        handleSetLanguage={lang => {
+          setLanguage(lang);
+          storeLanguageInLocalStorage(lang);
+        }}
+      />
     </MuiThemeProvider>
   );
 };
